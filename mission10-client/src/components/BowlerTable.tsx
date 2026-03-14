ca375eee21eeba50e859c8803557a9c4e190ab9b
BrodieJNR <Brodiej_Newman@hotmail.com>
2026-03-14 15:26:52 -0600

    bowlertable

--- mission10-client/src/components/BowlerTable.tsx
+++ mission10-client/src/components/BowlerTable.tsx
@@ -1,40 +1,40 @@
 ﻿import { Bowler } from "../models/Bowler";
 
-type Props = {
-    bowlers: Bowler[];
-};
+interface Props {
+  bowlers: Bowler[];
+}
 
-const BowlerTable = ({ bowlers }: Props) => {
-    return (
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th>Bowler Name</th>
-                    <th>Team</th>
-                    <th>Address</th>
-                    <th>City</th>
-                    <th>State</th>
-                    <th>Zip</th>
-                    <th>Phone</th>
-                </tr>
-            </thead>
-            <tbody>
-                {bowlers.map((b) => (
-                    <tr key={b.bowlerId}>
-                        <td>
-                            {b.firstName} {b.middleInit && b.middleInit + " "} {b.lastName}
-                        </td>
-                        <td>{b.teamName}</td>
-                        <td>{b.address}</td>
-                        <td>{b.city}</td>
-                        <td>{b.state}</td>
-                        <td>{b.zip}</td>
-                        <td>{b.phoneNumber}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
-    );
-};
-
-export default BowlerTable;
+export default function BowlerTable({ bowlers }: Props) {
+  return (
+    <table border={1} cellPadding="10">
+      <thead>
+        <tr>
+          <th>First Name</th>
+          <th>Middle Init</th>
+          <th>Last Name</th>
+          <th>Team</th>
+          <th>Address</th>
+          <th>City</th>
+          <th>State</th>
+          <th>Zip</th>
+          <th>Phone</th>
+        </tr>
+      </thead>
+      <tbody>
+        {bowlers.map((bowler) => (
+          <tr key={bowler.bowlerId}>
+            <td>{bowler.bowlerFirstName}</td>
+            <td>{bowler.bowlerMiddleInit}</td>
+            <td>{bowler.bowlerLastName}</td>
+            <td>{bowler.team.teamName}</td>
+            <td>{bowler.bowlerAddress}</td>
+            <td>{bowler.bowlerCity}</td>
+            <td>{bowler.bowlerState}</td>
+            <td>{bowler.bowlerZip}</td>
+            <td>{bowler.bowlerPhoneNumber}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
