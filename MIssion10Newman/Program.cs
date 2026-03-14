58ca1924a55a0a502e7973560cb4584ba718049a
BrodieJNR <Brodiej_Newman@hotmail.com>
2026-03-14 15:49:15 -0600

    fixes2

--- MIssion10Newman/Program.cs
+++ MIssion10Newman/Program.cs
@@ -3,7 +3,6 @@ using MIssion10Newman.Models;
 
 var builder = WebApplicationBuilder.CreateBuilder(args);
 
-// Add CORS service
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp",
@@ -15,34 +14,23 @@ builder.Services.AddCors(options =>
         });
 });
 
-// Add services to the container.
 builder.Services.AddControllers();
-
-// Add DbContext
 builder.Services.AddDbContext<BowlingLeagueContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("BowlingLeagueConnection")
-        ?? throw new InvalidOperationException("Connection string 'BowlingLeagueConnection' not found.")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("BowlingLeagueConnection")));
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Use CORS middleware
 app.UseCors("AllowReactApp");
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
