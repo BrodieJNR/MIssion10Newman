f6b268779ee5d8b59ea2bbcbe8b16e47e41c11bc
BrodieJNR <Brodiej_Newman@hotmail.com>
2026-03-14 15:33:22 -0600

    next update

--- MIssion10Newman/data/Program.cs
+++ MIssion10Newman/data/Program.cs
@@ -1,41 +1,49 @@
 using Microsoft.EntityFrameworkCore;
 using MIssion10Newman.Models;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplicationBuilder.CreateBuilder(args);
+
+// Add CORS service
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
 
-// Add DbContext using SQLite (already there)
+// Add DbContext
 builder.Services.AddDbContext<BowlingLeagueContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("BowlingConnection")));
+    options.UseSqlite(builder.Configuration.GetConnectionString("BowlingLeagueConnection")
+        ?? throw new InvalidOperationException("Connection string 'BowlingLeagueConnection' not found.")));
 
-// ADD THIS CORS BLOCK:
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("ReactPolicy", policy =>
-    {
-        policy.WithOrigins("http://localhost:3000")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
-});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// Use CORS middleware
+app.UseCors("AllowReactApp");
 
-// ADD THIS LINE (CORS must come before UseAuthorization):
-app.UseCors("ReactPolicy");
+app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
